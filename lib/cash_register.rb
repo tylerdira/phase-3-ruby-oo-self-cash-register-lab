8d813a9b9c236a538a0e28ad6abf77dc9f4c861d
Joseph Tyler DiRaffaele <tylerdiraffaele@gmail.com>
2022-09-21 15:43:42 -0700

    commit tree 9244ff711b172ed110a6738837a1ea08591ac786

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
 
+class CashRegister
+
+    attr_accessor :discount, :total, :items, :last_transaction
+
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @items = []
+    end
+
+    def add_item(title, price, quantity = 1)
+        @total += price * quantity
+        quantity.times do
+            @items << title
+        end
+    end
+
+    def apply_discount
+        if discount > 0
+            percent_discount = (100 - self.discount.to_f) / 100
+            self.total = (percent_discount * self.total).to_i
+            "After the discount, the total comes to $#{self.total}."
+        else 
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        self.total -= self.last_transaction
+    end
+
+
+    
+end
